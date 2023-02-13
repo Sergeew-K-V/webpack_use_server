cfce691d37d0a1eac6eb5bb3ad9b96dba3290f78
kirill.sergeev <kirill.sergeev@progforce.com>
2023-02-13 12:41:35 +0300

    message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -10,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running`);
 });
